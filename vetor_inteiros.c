57ac7e399b53505a0ef1d8a7d6e6c3ba5e348d83
Nicole Ernesto Roque <Nicoleroque775@gmail.com>
2026-08-25 22:37:47 +0000

    novos comentários

--- vetor_inteiros.c
+++ vetor_inteiros.c
@@ -1,10 +1,13 @@
+/*Escreva um programa que leia um vetor de inteiros, descubra e exiba um número inteiro 
+correspondente à quantidade de valores que aparecem mais de uma vez. Exemplo:
+se o vetor for [1, 2, 3, 4, 5, 3, 6, 7, 2, 8], o retorno será 2 (2 números se repetem, 2 e 3). */
 #include <stdio.h>
 
 int main() {
   int vet[10] = {1, 2, 3, 4, 5, 3, 6, 7, 2, 3};//se não colocar o tamanho do vetor ele cria um vetor com o tamanho das quantidade do vetor
 
-  for (int i = 0; i < 10; i++) printf("%d ", vet[i]); //exibe o vetor
-  printf("\n");
+  for (int i = 0; i < 10; i++) printf("%d ", vet[i]); // for de uma linha, exibe as 10 posições do vetor 
+  printf("\n"); //pula uma linha
 
   int totalRepet = 0; //declara a variavel e inicializa
   for (int i = 0; i < 9; i++) { //anda até a ante-penultima posição
